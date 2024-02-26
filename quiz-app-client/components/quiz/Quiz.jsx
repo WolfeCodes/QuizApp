3e2ef7bc6b752316b329b3afb9785633c3ff4a9a
unknown <marc.wolfe21@gmail.com>
2024-02-26 00:14:51 -0600

    Add functions for handling the next and previous question

--- quiz-app-client/components/quiz/Quiz.jsx
+++ quiz-app-client/components/quiz/Quiz.jsx
@@ -47,73 +47,106 @@ const Quiz = () => {
         ? answer.map((a) => a.charAt(0))
         : answer.charAt(0);
 
-        if(existingAnswerIndex !== -1){
-            const updatedAnswers = [...prevAnswers]
-            updatedAnswers[existingAnswerIndex] = {id: questionId, answer: selectedAnswer}
-            return updatedAnswers
-        }else {
-            const newAnswer = {id: questionId, answer: selectedAnswer}
-            return [...prevAnswers, newAnswer]
-        }
+      if (existingAnswerIndex !== -1) {
+        const updatedAnswers = [...prevAnswers];
+        updatedAnswers[existingAnswerIndex] = {
+          id: questionId,
+          answer: selectedAnswer,
+        };
+        return updatedAnswers;
+      } else {
+        const newAnswer = { id: questionId, answer: selectedAnswer };
+        return [...prevAnswers, newAnswer];
+      }
     });
   };
 
-
   const isChecked = (questionId, choice) => {
-    const selectedAnswer = selectedAnswers.find((answer) => answer.id === questionId)
-    if(!selectedAnswer){
-        return false
+    const selectedAnswer = selectedAnswers.find(
+      (answer) => answer.id === questionId
+    );
+    if (!selectedAnswer) {
+      return false;
     }
-    if(Array.isArray(selectedAnswer.answer)){
-        return selectedAnswer.answer.includes(choice.charAt(0))
+    if (Array.isArray(selectedAnswer.answer)) {
+      return selectedAnswer.answer.includes(choice.charAt(0));
     }
-    return selectedAnswer.answer === choice.charAt(0)
-  }
+    return selectedAnswer.answer === choice.charAt(0);
+  };
 
   const handleCheckboxChange = (questionId, choice) => {
     setSelectedAnswers((prevAnswers) => {
-        const existingAnswerIndex = prevAnswers.findIndex((answerObj) => answerObj.id === questionId)
-        const selectedAnswer = Array.isArray(choice) ? choice.map((c) => c.charAt(0)) : choice.charAt(0)
+      const existingAnswerIndex = prevAnswers.findIndex(
+        (answerObj) => answerObj.id === questionId
+      );
+      const selectedAnswer = Array.isArray(choice)
+        ? choice.map((c) => c.charAt(0))
+        : choice.charAt(0);
 
-        if(!+ existingAnswerIndex -1){
-            const updatedAnswers = [...prevAnswers]
-            const existingAnswers = updatedAnswers[existingAnswerIndex].answer
-            let newAnswer 
-            if(Array.isArray(existingAnswers)){
-                newAnswer = existingAnswers.includes(selectedAnswer) ? existingAnswers.filter((a) => a !== selectedAnswer)
-                :[...existingAnswers, selectedAnswer]
-            }else{
-                newAnswer = [existingAnswers, selectedAnswer]
-            }
-            updatedAnswers[existingAnswerIndex] = {id: questionId, answer: newAnswer}
-            return updatedAnswers
-        }else{
-            const newAnswer = {id: questionId, answer: [selectedAnswer]}
-            return [...prevAnswers, newAnswer]
+      if (!+existingAnswerIndex - 1) {
+        const updatedAnswers = [...prevAnswers];
+        const existingAnswers = updatedAnswers[existingAnswerIndex].answer;
+        let newAnswer;
+        if (Array.isArray(existingAnswers)) {
+          newAnswer = existingAnswers.includes(selectedAnswer)
+            ? existingAnswers.filter((a) => a !== selectedAnswer)
+            : [...existingAnswers, selectedAnswer];
+        } else {
+          newAnswer = [existingAnswers, selectedAnswer];
         }
-    })
-  }
+        updatedAnswers[existingAnswerIndex] = {
+          id: questionId,
+          answer: newAnswer,
+        };
+        return updatedAnswers;
+      } else {
+        const newAnswer = { id: questionId, answer: [selectedAnswer] };
+        return [...prevAnswers, newAnswer];
+      }
+    });
+  };
 
   const handleSubmit = () => {
     let scores = 0;
     quizQuestions.forEach((question) => {
-        const selectedAnswer = selectedAnswers.find((answer) => answer.id === question.id)
-        if(selectedAnswer){
-            const selectedOptions = Array.isArray(selectedAnswer.answer) ? selectedAnswer.answer : [selectedAnswer.answer]
-            const correctOptions = Array.isArray(question.correctAnswers) ? question.correctAnswers : [question.correctAnswers]
-            const isCorrect = selectedOptions.every((option) => correctOptions.includes(option))
-            if(isCorrect){
-                scores ++
-            }
+      const selectedAnswer = selectedAnswers.find(
+        (answer) => answer.id === question.id
+      );
+      if (selectedAnswer) {
+        const selectedOptions = Array.isArray(selectedAnswer.answer)
+          ? selectedAnswer.answer
+          : [selectedAnswer.answer];
+        const correctOptions = Array.isArray(question.correctAnswers)
+          ? question.correctAnswers
+          : [question.correctAnswers];
+        const isCorrect = selectedOptions.every((option) =>
+          correctOptions.includes(option)
+        );
+        if (isCorrect) {
+          scores++;
         }
-    })
-    setTotalScores(scores)
-    setSelectedAnswers([{id: "", answer: [""]}])
-    setCurrentQuestionIndex(0)
-    navigate("/quiz-result", {state:{quizQuestions, totalScores: scores}})
-  }
-  
+      }
+    });
+    setTotalScores(scores);
+    setSelectedAnswers([{ id: "", answer: [""] }]);
+    setCurrentQuestionIndex(0);
+    navigate("/quiz-result", { state: { quizQuestions, totalScores: scores } });
+  };
 
+  const handleNextQuestion = () => {
+    if(currentQuestionIndex < quizQuestions.length -1){
+        setCurrentQuestionIndex((prevIndex) => prevIndex + 1)
+    }
+    else{
+        handleSubmit()
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if(currentQuestionIndex > 0){
+        setCurrentQuestionIndex((prevIndex) => prevIndex - 1)
+    }
+  };
 
   return <div>Quiz</div>;
 };
