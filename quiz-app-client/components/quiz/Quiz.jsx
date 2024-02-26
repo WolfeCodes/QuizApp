c608dddb6a497dd320d7ed12a9047b3f4e9760a4
unknown <marc.wolfe21@gmail.com>
2024-02-25 21:55:34 -0600

    Add functionality for fetching all quiz data and handling selected answer change

--- quiz-app-client/components/quiz/Quiz.jsx
+++ quiz-app-client/components/quiz/Quiz.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Quiz = () => {
-  return (
-    <div>Quiz</div>
-  )
-}
+  const [quizQuestions, setQuizQuestions] = useState([
+    {
+      id: "",
+      correctAnswers: "",
+      question: "",
+      questionType: "",
+    },
+  ]);
 
-export default Quiz
+  const [selectedAnswer, setSelectedAnswer] = useState([
+    {
+      id: "",
+      answer: "",
+    },
+  ]);
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [totalScores, setTotalScores] = useState(0);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { selectedSubject, selectedNumOfQuestions } = location.state;
+
+  useEffect(() => {
+    fetchQuizData();
+  }, []);
+
+  const fetchQuizData = async () => {
+    if (selectedNumOfQuestions && selectedSubject) {
+      const questions = await fetchQuizForUser(
+        selectedNumOfQuestions,
+        selectedSubject
+      );
+      setQuizQuestions(questions);
+    }
+  };
+
+  const handleAnswerChange = (questionId, answer) => {
+    setSelectedAnswer((prevAnswers) => {
+        const existingAnswerIndex = prevAnswers.findIndex((answerObj) => answerObj.id === questionId)
+    })
+  }
+
+  return <div>Quiz</div>;
+};
+
+export default Quiz;
